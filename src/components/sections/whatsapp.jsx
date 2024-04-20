698e17a15eee8d6aef1a367452d8fb22b4e131dc
IshanBh7 <113094607+IshanBh7@users.noreply.github.com>
2024-04-20 17:18:32 +0530

    Update whatsapp.jsx

--- src/components/sections/whatsapp.jsx
+++ src/components/sections/whatsapp.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export default function App(){
   return(
-<div className="whatsapp" style={{ position: "fixed", bottom: "20px", right: "20px", zIndex: "6" }}>
+<div className="whatsapp" style={{ position: "fixed", bottom: "50px", left: "40px", zIndex: "6" }}>
   <a href="https://wa.me/919928498990?text=Hello! I am here to enquire about the admission process." target="_blank">
   <Image
                 alt={'whatsapp-icon'}
